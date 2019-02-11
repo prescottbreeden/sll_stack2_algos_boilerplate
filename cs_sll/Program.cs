4b633091530a1b315cb82978f676fcb608baab3f
prescottbreeden <prescott.breeden@gmail.com>
2019-02-11 09:04:33 -0800

    initial commit

---
+++ cs_sll/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cs_sll
+{
+    class Node
+    {
+        // complete the class
+
+    }
+    class SLL
+    {
+        // complete the class
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SLL sll = new SLL();
+
+            // execute your sll methods here
+        }
+    }
+}
---
+++ js_sll/sll.js
@@ -0,0 +1,13 @@
+function sll() {
+  this.head = null;
+}
+
+function node(val) {
+  // complete the class
+}
+
+sll.prototype.addToFront = function() {
+  //method logic here 
+}
+
+// execute this file in your terminal by running 'node sll.js'
